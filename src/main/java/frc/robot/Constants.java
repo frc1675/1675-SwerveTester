50ac8e5529a68fd1831afcb96d7e4df6fb2f4ec7
Jake Herrmann <herrmann.jake@protonmail.com>
2023-11-16 19:27:02 -0600

    Changed CAN IDs to correct value

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,36 +12,35 @@ public class Constants {
     public static final double SWERVE_I = 0;
     public static final double SWERVE_D = 0;
 
-    //TODO correct IDs!
     public class FrontRight {
         private FrontRight(){}
 
-        public static final int CANCODER_ID = 1;
-        public static final int DRIVE_MOTOR_ID = 2;
-        public static final int STEER_MOTOR_ID = 3;
+        public static final int CANCODER_ID = 12;
+        public static final int DRIVE_MOTOR_ID = 3;
+        public static final int STEER_MOTOR_ID = 4;
     }
 
     public class FrontLeft {
         private FrontLeft(){}
 
-        public static final int CANCODER_ID = 4;
-        public static final int DRIVE_MOTOR_ID = 5;
-        public static final int STEER_MOTOR_ID = 6;
+        public static final int CANCODER_ID = 10;
+        public static final int DRIVE_MOTOR_ID = 1;
+        public static final int STEER_MOTOR_ID = 2;
     }
 
     public class BackRight {
         private BackRight(){}
 
-        public static final int CANCODER_ID = 7;
-        public static final int DRIVE_MOTOR_ID = 8;
-        public static final int STEER_MOTOR_ID = 9;
+        public static final int CANCODER_ID = 11;
+        public static final int DRIVE_MOTOR_ID = 7;
+        public static final int STEER_MOTOR_ID = 8;
     }
 
     public class BackLeft {
         private BackLeft(){}
 
-        public static final int CANCODER_ID = 10;
-        public static final int DRIVE_MOTOR_ID = 11;
-        public static final int STEER_MOTOR_ID = 12;
+        public static final int CANCODER_ID = 9;
+        public static final int DRIVE_MOTOR_ID = 5;
+        public static final int STEER_MOTOR_ID = 6;
     }
 }
